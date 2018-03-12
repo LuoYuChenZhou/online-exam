7fc85000c914666d4e1c3a4362c518eb4353a2e8
caixuanxu <32160289+caixuanxu@users.noreply.github.com>
2018-03-12 09:28:49 +0800

    添加我的学生列表查询接口

--- src/main/java/com/lycz/dao/ExamineeMapper.java
+++ src/main/java/com/lycz/dao/ExamineeMapper.java
@@ -1,9 +1,18 @@
 package com.lycz.dao;
 
 import com.lycz.model.Examinee;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.stereotype.Repository;
 import tk.mybatis.mapper.common.Mapper;
 
+import java.util.List;
+import java.util.Map;
+
 @Repository
 public interface ExamineeMapper extends Mapper<Examinee> {
+
+    List<Map<String, Object>> searchExamineeInfo(@Param("searchClass") String searchClass,
+                                                 @Param("examineeName") String examineeName,
+                                                 @Param("examineeNum")String examineeNum
+    );
 }
--- src/main/java/com/lycz/service/user/impl/ExamineeServiceImpl.java
+++ src/main/java/com/lycz/service/user/impl/ExamineeServiceImpl.java
@@ -1,6 +1,9 @@
 package com.lycz.service.user.impl;
 
+import com.github.pagehelper.PageHelper;
+import com.lycz.controller.common.FixPageInfo;
 import com.lycz.controller.common.ToolUtil;
+import com.lycz.dao.ExamineeMapper;
 import com.lycz.model.Examinee;
 import com.lycz.service.base.impl.BaseServiceTk;
 import com.lycz.service.user.ExamineeService;
@@ -10,10 +13,12 @@ import org.springframework.transaction.annotation.Transactional;
 import tk.mybatis.mapper.entity.Example;
 
 import java.util.List;
+import java.util.Map;
 
 @Service
 @Transactional(propagation = Propagation.REQUIRED, rollbackFor = Exception.class)
 public class ExamineeServiceImpl extends BaseServiceTk<Examinee> implements ExamineeService {
+    private ExamineeMapper examineeMapper;
 
     @Override
     public Examinee eeLogin(String userName, String password) {
@@ -30,5 +35,16 @@ public class ExamineeServiceImpl extends BaseServiceTk<Examinee> implements Exam
         example.or().andEqualTo("loginName", userName).andNotEqualTo("status", "4");
         List<Examinee> eeList = selectByExample(example);
         return ToolUtil.isNotEmpty(eeList);
+    }
+
+    @Override
+    public FixPageInfo<Map<String, Object>> searchExamineeInfo(String searchClass, String examineeName, String examineeNum,Integer page,Integer limit) {
+        PageHelper.startPage(page, limit);
+        List<Map<String, Object>> gardeList = examineeMapper.searchExamineeInfo(searchClass,examineeName,examineeNum);
+        if (ToolUtil.isEmpty(gardeList)) {
+            return null;
+        } else {
+            return new FixPageInfo<>(gardeList);
+        }
     }
 }
