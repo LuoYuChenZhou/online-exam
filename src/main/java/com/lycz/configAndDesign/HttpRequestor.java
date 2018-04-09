7fa8f495f001ac4c2ffdfabe943419570920c017
LuoYuChenZhou <32658637+LuoYuChenZhou@users.noreply.github.com>
2018-04-09 11:07:15 +0800

    1

---
+++ src/main/java/com/lycz/configAndDesign/HttpRequestor.java
@@ -0,0 +1,350 @@
+package com.lycz.configAndDesign;
+
+import java.io.*;
+import java.net.*;
+import java.security.MessageDigest;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+
+public class HttpRequestor {
+
+    private String charset = "UTF-8";
+    private Integer connectTimeout = null;
+    private Integer socketTimeout = null;
+    private String proxyHost = null;
+    private Integer proxyPort = null;
+
+    /**
+     * Do GET request
+     *
+     * @param url
+     * @return
+     * @throws Exception
+     * @throws IOException
+     */
+    public String doGet(String url) throws Exception {
+        URL localURL = new URL(url);
+        URLConnection connection = this.openConnection(localURL);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) connection;
+        httpURLConnection.setRequestProperty("Accept-Charset", charset);
+        httpURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded");
+        InputStream inputStream = null;
+        InputStreamReader inputStreamReader = null;
+        BufferedReader reader = null;
+        StringBuffer resultBuffer = new StringBuffer();
+        String tempLine = null;
+        //响应失败
+        if (httpURLConnection.getResponseCode() >= 300) {
+            throw new Exception("HTTP Request is not success, Response code is " + httpURLConnection.getResponseCode());
+        }
+
+        try {
+            inputStream = httpURLConnection.getInputStream();
+            inputStreamReader = new InputStreamReader(inputStream);
+            reader = new BufferedReader(inputStreamReader);
+
+            while ((tempLine = reader.readLine()) != null) {
+                resultBuffer.append(tempLine);
+            }
+
+        } finally {
+
+            if (reader != null) {
+                reader.close();
+            }
+
+            if (inputStreamReader != null) {
+                inputStreamReader.close();
+            }
+
+            if (inputStream != null) {
+                inputStream.close();
+            }
+
+        }
+
+        return resultBuffer.toString();
+    }
+
+    /**
+     * Do POST request
+     *
+     * @param url
+     * @param parameterMap
+     * @return
+     * @throws Exception
+     */
+    public String doPost(String url, Map parameterMap) throws Exception {
+        
+        /* Translate parameter map to parameter date string */
+        StringBuffer parameterBuffer = new StringBuffer();
+        if (parameterMap != null) {
+            Iterator iterator = parameterMap.keySet().iterator();
+            String key = null;
+            String value = null;
+            while (iterator.hasNext()) {
+                key = (String) iterator.next();
+                if (parameterMap.get(key) != null) {
+                    value = (String) parameterMap.get(key);
+                } else {
+                    value = "";
+                }
+
+                parameterBuffer.append(key).append("=").append(value);
+                if (iterator.hasNext()) {
+                    parameterBuffer.append("&");
+                }
+            }
+        }
+
+        System.out.println("POST parameter : " + parameterBuffer.toString());
+
+        URL localURL = new URL(url);
+
+        URLConnection connection = this.openConnection(localURL);
+        HttpURLConnection httpURLConnection = (HttpURLConnection) connection;
+
+        httpURLConnection.setDoOutput(true);
+        httpURLConnection.setRequestMethod("POST");
+        httpURLConnection.setRequestProperty("Accept-Charset", charset);
+        httpURLConnection.setRequestProperty("Content-Type", "application/x-www-form-urlencoded;charset=UTF-8");
+        httpURLConnection.setRequestProperty("Content-Length", String.valueOf(parameterBuffer.length()));
+        //httpURLConnection.setD
+        OutputStream outputStream = null;
+        OutputStreamWriter outputStreamWriter = null;
+        InputStream inputStream = null;
+        InputStreamReader inputStreamReader = null;
+        BufferedReader reader = null;
+        StringBuffer resultBuffer = new StringBuffer();
+        String tempLine = null;
+
+        try {
+            outputStream = httpURLConnection.getOutputStream();
+//            outputStreamWriter = new OutputStreamWriter(outputStream);
+
+//            outputStreamWriter.write(parameterBuffer.toString());
+            PrintWriter out = new PrintWriter(new OutputStreamWriter(outputStream, "utf-8"));
+            out.println(parameterBuffer.toString());
+            out.flush();
+//            outputStreamWriter.flush();
+            //响应失败
+            if (httpURLConnection.getResponseCode() >= 300) {
+                throw new Exception("HTTP Request is not success, Response code is " + httpURLConnection.getResponseCode());
+            }
+            //接收响应流
+            inputStream = httpURLConnection.getInputStream();
+            inputStreamReader = new InputStreamReader(inputStream, charset);
+            reader = new BufferedReader(inputStreamReader);
+
+            while ((tempLine = reader.readLine()) != null) {
+                resultBuffer.append(tempLine);
+            }
+
+        } finally {
+
+            if (outputStreamWriter != null) {
+                outputStreamWriter.close();
+            }
+
+            if (outputStream != null) {
+                outputStream.close();
+            }
+
+            if (reader != null) {
+                reader.close();
+            }
+
+            if (inputStreamReader != null) {
+                inputStreamReader.close();
+            }
+
+            if (inputStream != null) {
+                inputStream.close();
+            }
+
+        }
+
+        return resultBuffer.toString();
+    }
+
+    /**
+     * Http Get 请求
+     */
+    public static String sendGet(String url, Map parameterMap) {
+        String result = "";
+        BufferedReader in = null;
+        StringBuffer parameterBuffer = new StringBuffer();
+        try {
+            if (parameterMap != null) {
+                Iterator iterator = parameterMap.keySet().iterator();
+                String key = null;
+                String value = null;
+                while (iterator.hasNext()) {
+                    key = (String) iterator.next();
+                    if (parameterMap.get(key) != null) {
+                        value = (String) parameterMap.get(key);
+                    } else {
+                        value = "";
+                    }
+
+                    parameterBuffer.append(key).append("=").append(value);
+                    if (iterator.hasNext()) {
+                        parameterBuffer.append("&");
+                    }
+                }
+            }
+            String urlNameString = url + "?" + parameterBuffer.toString();
+            System.out.println(urlNameString);
+            URL realUrl = new URL(urlNameString);
+            // 打开和URL之间的连接
+            URLConnection connection = realUrl.openConnection();
+            // 设置通用的请求属性
+            connection.setRequestProperty("Accept-Charset", "UTF-8");
+            connection.setRequestProperty("accept", "*/*");
+            connection.setRequestProperty("connection", "Keep-Alive");
+            connection.setRequestProperty("user-agent", "Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1;SV1)");
+            // 建立实际的连接
+            connection.connect();
+            // 获取所有响应头字段
+            Map<String, List<String>> map = connection.getHeaderFields();
+            // 遍历所有的响应头字段
+//            for (String key : map.keySet()) {
+//                System.out.println(key + "--->" + map.get(key));
+//            }
+            // 定义 BufferedReader输入流来读取URL的响应
+            in = new BufferedReader(new InputStreamReader(
+                    connection.getInputStream(), "UTF-8"));
+            String line;
+            while ((line = in.readLine()) != null) {
+                result += line;
+            }
+        } catch (Exception e) {
+            System.out.println("发送GET请求出现异常！" + e);
+            e.printStackTrace();
+        }
+        // 使用finally块来关闭输入流
+        finally {
+            try {
+                if (in != null) {
+                    in.close();
+                }
+            } catch (Exception e2) {
+                e2.printStackTrace();
+            }
+        }
+        return result;
+    }
+
+    private URLConnection openConnection(URL localURL) throws IOException {
+        URLConnection connection;
+        if (proxyHost != null && proxyPort != null) {
+            Proxy proxy = new Proxy(Proxy.Type.HTTP, new InetSocketAddress(proxyHost, proxyPort));
+            connection = localURL.openConnection(proxy);
+        } else {
+            connection = localURL.openConnection();
+        }
+        return connection;
+    }
+
+    /**
+     * Render request according setting
+     *
+     * @param
+     */
+    private void renderRequest(URLConnection connection) {
+
+        if (connectTimeout != null) {
+            connection.setConnectTimeout(connectTimeout);
+        }
+
+        if (socketTimeout != null) {
+            connection.setReadTimeout(socketTimeout);
+        }
+
+    }
+
+    /*
+     * Getter & Setter
+     */
+    public Integer getConnectTimeout() {
+        return connectTimeout;
+    }
+
+    public void setConnectTimeout(Integer connectTimeout) {
+        this.connectTimeout = connectTimeout;
+    }
+
+    public Integer getSocketTimeout() {
+        return socketTimeout;
+    }
+
+    public void setSocketTimeout(Integer socketTimeout) {
+        this.socketTimeout = socketTimeout;
+    }
+
+    public String getProxyHost() {
+        return proxyHost;
+    }
+
+    public void setProxyHost(String proxyHost) {
+        this.proxyHost = proxyHost;
+    }
+
+    public Integer getProxyPort() {
+        return proxyPort;
+    }
+
+    public void setProxyPort(Integer proxyPort) {
+        this.proxyPort = proxyPort;
+    }
+
+    public String getCharset() {
+        return charset;
+    }
+
+    public void setCharset(String charset) {
+        this.charset = charset;
+    }
+
+    //MD5加密
+    public static String string2MD5(String inStr) {
+        MessageDigest md5 = null;
+        try {
+            md5 = MessageDigest.getInstance("MD5");
+        } catch (Exception e) {
+            System.out.println(e.toString());
+            e.printStackTrace();
+            return "";
+        }
+        char[] charArray = inStr.toCharArray();
+        byte[] byteArray = new byte[charArray.length];
+
+        for (int i = 0; i < charArray.length; i++)
+            byteArray[i] = (byte) charArray[i];
+        byte[] md5Bytes = md5.digest(byteArray);
+        StringBuffer hexValue = new StringBuffer();
+        for (int i = 0; i < md5Bytes.length; i++) {
+            int val = ((int) md5Bytes[i]) & 0xff;
+            if (val < 16)
+                hexValue.append("0");
+            hexValue.append(Integer.toHexString(val));
+        }
+        return hexValue.toString();
+
+    }
+
+    /**
+     * 加密解密算法 执行一次加密，两次解密
+     */
+    public static String convertMD5(String inStr) {
+
+        char[] a = inStr.toCharArray();
+        for (int i = 0; i < a.length; i++) {
+            a[i] = (char) (a[i] ^ 't');
+        }
+        String s = new String(a);
+        return s;
+
+    }
+}
