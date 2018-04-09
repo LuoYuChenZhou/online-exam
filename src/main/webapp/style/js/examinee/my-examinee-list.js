b272207b9fc997471838deb1ca1b859878bd0807
LuoYuChenZhou <32658637+LuoYuChenZhou@users.noreply.github.com>
2018-04-09 11:09:53 +0800

    1、修改swagger样式 2、初步编辑我的考生

--- src/main/webapp/style/js/examinee/my-examinee-list.js
+++ src/main/webapp/style/js/examinee/my-examinee-list.js
@@ -1,22 +1,26 @@
+let curChooseGradeId;//当前选中的班级id
+
 layui.use('form', function () {
     let form = layui.form;
 
     $.get("/Grade/getGradeListByNameUser.do", {page: 1, limit: 1000, token: token}, function (data) {
-        $("#searchClass").empty();
+        let searchClass = $("#searchClass");
+        searchClass.empty();
         if (data.code === 0) {
-            $("#searchClass").append("<option value=\"\"></option>");
+            searchClass.append("<option value=\"allGrade\">所有</option>");
 
             $.each(data.data, function (index, val) {
-                $("#searchClass").append("<option value=" + val.id + ">" + val.gradeName + "</option>");
+                searchClass.append("<option value=" + val.id + ">" + val.gradeName + "</option>");
             });
         } else if (data.code === 204) {
-            $("#searchClass").html("<option value=\"\"></option>");
+            searchClass.html("<option value=\"allGrade\">所有</option>");
         }
         //如果将下面这句话放到方法外面，需要注意异步问题导致的页面渲染失败
         form.render('select');
     });
 
-    form.on('select(searchClass)', function () {
+    form.on('select(searchClass)', function (data) {
+        curChooseGradeId = data.value;
         searchBtnClick();
     });
 });
@@ -26,18 +30,18 @@ layui.use('table', function () {
     let table = layui.table;
 
     table.render({
-        elem: '#log_table'
+        elem: '#main_table'
         , height: 'full-80'//高度设置为距底部30
-        , url: '../../SysLog/getSysLogList.do' //数据接口
+        , url: '../../EeGrade/getEeListByNameNoClass.do' //数据接口
         , page: true //开启分页
         , cols: [[ //表头
-            {field: 'logTitle', title: '标题', width: 180, fixed: 'left'}
-            , {field: 'moduleName', title: '模块', width: 200}
-            , {field: 'logLevel', title: '等级', width: 100, templet: '#levelName'}
-            , {field: 'logDescription', title: '描述', width: 350}
-            , {field: 'createTime', title: '发生时间', width: 177, templet: '#time2string'}
-            , {field: 'logUser', title: '操作人', width: 120}
-            , {fixed: 'right', title: '操作', width: 100, align: 'center', toolbar: '#log_bar'}
+            {field: 'realName', title: '考生姓名', width: 120, sort: true, fixed: 'left'}
+            , {field: 'eeNo', title: '考生号', width: 200}
+            , {field: 'sex', title: '性别', width: 80, templet: '#num2Sex'}
+            , {field: 'phone', title: '联系电话', width: 180}
+            , {field: 'email', title: '电子邮箱', width: 240}
+            , {field: 'gradeNames', title: '所在班级', width: 240}
+            , {title: '操作', width: 100, align: 'center', toolbar: '#main_table_bar'}
         ]]
         , id: 'logTable'
         , where: {
@@ -46,12 +50,12 @@ layui.use('table', function () {
     });
 
     //监听工具条
-    table.on('tool(log-t)', function (obj) { //注：tool是工具条事件名，test是table原始容器的属性 lay-filter="对应的值"
+    table.on('tool(main_table_filter)', function (obj) { //注：tool是工具条事件名，test是table原始容器的属性 lay-filter="对应的值"
         let data = obj.data; //获得当前行数据
         let layEvent = obj.event; //获得 lay-event 对应的值（也可以是表头的 event 参数对应的值）
         // let tr = obj.tr; //获得当前行 tr 的DOM对象
 
-        if (layEvent === 'detail') { //查看
+        if (layEvent === 'addToGrade') { //添加到班级
             layui.use('layer', function () {
                 layer.open({
                     type: 0,
@@ -71,16 +75,14 @@ layui.use('table', function () {
 
 //搜索按钮点击
 function searchBtnClick() {
-    let jsonFormData = getEntity("#log_form");
+    let jsonFormData = getEntity("#search_form");
     let table = layui.table;
 
     table.reload('logTable', {
         where: {
             token: token
-            , searchStartTime: jsonFormData.searchStartTime
-            , searchEndTime: jsonFormData.searchEndTime
-            , searchLevel: jsonFormData.searchLevel
-            , searchTitle: jsonFormData.searchTitle
+            , searchClass: curChooseGradeId
+            , searchString: jsonFormData.searchString
         },
         page: {
             curr: 1
